1573eff259e68ea55e388beee5972fadcff4fd1e
MaYiLagann <lhwshy@naver.com>
2018-01-11 22:22:36 +0900

    시간 시스템 추가, 개발자 도구 UI 추가, 배경 추가

---
+++ BlackOut/Assets/1. Scripts/Scripts GameScene/GameSceneUiCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Canvas))]
+public class GameSceneUiCtrl : MonoBehaviour {
+
+	[Header("- Dev Settings -")]
+	public Text CurrentDayProgressText;
+	public Text CurrentDaySurvivesText;
+
+	[Header("- Require Components -")]
+	public GameSystemCtrl CurrentGameSystemCtrl;
+
+	/* Event Functions */
+
+	void Update() {
+		SetUiValues();
+	}
+	
+	/* Event Functions */
+
+	/* Logic Functions */
+
+	public void SetAnim(int state) {
+		Animator anim = gameObject.GetComponent<Animator>();
+		anim.SetInteger("State", state);
+		anim.SetTrigger("Move");
+	}
+
+	private void SetUiValues() {
+		CurrentDayProgressText.text = "하루 진행도: " + (CurrentGameSystemCtrl.getDayProgress() / CurrentGameSystemCtrl.DaySeconds * 100).ToString("F2") + "%";
+		CurrentDaySurvivesText.text = "진행된 일수: " + CurrentGameSystemCtrl.getDaySurvives() + "일";
+	}
+
+	/* Logic Functions */
+}
---
+++ BlackOut/Assets/1. Scripts/Scripts GameScene/GameSystemCtrl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystemCtrl : MonoBehaviour {
+
+	[Header("- Time Settings -")]
+	public float DaySeconds = 600f;
+
+	private bool dayRun;
+	private int daySurvives;
+	private float dayProgress;
+
+	// [Header("- Upgrade Settings -")]
+	// public UpgradeBuilding[] MaxBuildingUpgrade;
+
+	/* Event Functions */
+
+	void Awake () {
+		dayRun = true;
+		daySurvives = 1;
+		dayProgress = 0f;
+	}
+	
+	void Update () {
+		RunTime();
+	}
+
+	/* Event Functions */
+
+	/* Get Functions */
+
+	public float getDayProgress() {
+		return dayProgress;
+	}
+
+	public int getDaySurvives() {
+		return daySurvives;
+	}
+
+	/* Get Functions */
+
+	/* Logic Functions */
+
+	private void RunTime() {
+		if(!dayRun) return;
+		dayProgress += Time.deltaTime;
+		if(dayProgress >= DaySeconds){
+			dayProgress -= DaySeconds;
+			daySurvives++;
+		}
+	}
+
+	/* Logic Functions */
+
+}
+
+// [SerializeField]
+// public struct UpgradeBuilding : MonoBehaviour {
+// 	float ObtainGoldSecond;
+// 	float ObtailGoldDay;
+// 	float CostGold;
+// 	float CostTime;
+// }
